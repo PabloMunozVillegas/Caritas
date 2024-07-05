5579a7f0e8e605b152f5e9410ef1e34f18289f89
PabloMunozVillegas <p.munoz@opti-system.com>
2024-07-05 17:03:47 -0400

    Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import GreenFace from './GreenFace';
+import GreenFace from './Faces3D/ExcelenteFace';
+import BlueFace from './Faces3D/MalaFace';
 
 function App() {
   return (
     <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <GreenFace />
+      <div onClick={() => alert('Carita Feliz')}>
+        <GreenFace/>
+      </div>
+      <div onClick={() => alert('Carita Triste')}>
+        <BlueFace />
+      </div>
     </div>
   );
 }
